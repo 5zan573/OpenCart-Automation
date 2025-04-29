975ed4211b546a5d10f84ce0d90f695fa87b4709
5zan573 <faizanhussain573@gmail.com>
2025-04-29 23:00:01 +0530

    practice

--- src/main/java/pageObjectstestCases/LoginTest.java
+++ src/main/java/pageObjectstestCases/LoginTest.java
@@ -1,5 +1,29 @@
 package pageObjectstestCases;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.homePage;
+import pageObjects.loginPage;
+import pageObjects.myaccountPage;
+
 public class LoginTest extends BaseClass {
 
+	@Test
+	public void LoggingInWithValidDetails() throws InterruptedException {
+		homePage hp=new homePage(driver);
+		hp.myaccount();
+		hp.Clickonlogin();
+		
+		loginPage lp=new loginPage(driver);
+		lp.setemail(p.getProperty("email"));
+		lp.setpassword(p.getProperty("password"));
+		lp.clickonlogin();
+		
+		myaccountPage ap=new myaccountPage(driver);
+		Thread.sleep(1000);
+		Assert.assertEquals(ap.MyaccountPageExists(), "My Account");
+		ap.logoutbtn();
+	}
+	
 }
